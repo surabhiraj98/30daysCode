97c25c5361abd7c45c82628bdeee013078f4bfd7
Surabhi Kumari <42448772+surabhiraj98@users.noreply.github.com>
2019-09-26 22:13:22 +0530

    Create Day10.java

---
+++ Day10.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+ public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+
+        int sum = 0, max = 0;
+        while (n > 0) {
+            if (n % 2 == 1) {
+                sum++;
+                if (sum > max) {
+                    max = sum;
+                }
+            } else {
+                sum = 0;
+            }
+            n = n / 2;
+        }
+        System.out.println(max);
+    }
+}
+
